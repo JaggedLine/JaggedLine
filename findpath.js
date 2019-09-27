50298e4bb778a7cf255edb1cb839c16e8b9045b6
kuyanov <43725417+kuyanov@users.noreply.github.com>
2019-09-27 19:33:31 +0300

    Добавил мега-идейное отсечение.

--- findpath.js
+++ findpath.js
@@ -1,31 +1,14 @@
 function fedroFindMax(event)
 {
-    let n, m, x1, y1, x2, y2;
-    let dirx, diry, pathx, pathy, anspathx, anspathy, used, used1, max_answer;
+    const off = 3;
+    let n, m, x1, y1, x2, y2, col, used, prec;
+    let depth = 0, global_ops = 0;
+    let dir = [[1, 2], [1, -2], [2, 1], [2, -1], [-1, 2], [-1, -2], [-2, 1], [-2, -1]];
+    let path = [];
+    let anspath = [];
 
     function crossProduct(x1, y1, x2, y2) {
         return x1 * y2 - x2 * y1;
-    }
-
-    function submitCurrentMax() {
-        let ans = [];
-        for (let id = 0; id < anspathx.length; id++) {
-            ans.push([anspathx[id], anspathy[id]]);
-        }
-        postMessage(ans);
-    }
-
-    function submitCurrent() {
-        let ans = [];
-        for (let id = 0; id < pathx.length; id++) {
-            ans.push([pathx[id], pathy[id]]);
-        }
-        postMessage(ans);
-    }
-
-    function finish() {
-        submitCurrentMax();
-        postMessage('finish');
     }
 
     function segmentsIntersect(x1, y1, x2, y2, x3, y3, x4, y4) {
@@ -42,10 +25,60 @@ function fedroFindMax(event)
         return false;
     }
 
-    function checkIntersection(lastx, lasty, nextx, nexty) {
-        for (let id = 0; id < pathx.length - 1; id++) {
-            if (segmentsIntersect(pathx[id], pathy[id], pathx[id + 1], pathy[id + 1], 
-                lastx, lasty, nextx, nexty)) {
+    function isCorrect(x, y) {
+        return x >= 0 && x < n && y >= 0 && y < m;
+    }
+
+    function init(data) {
+        n = data[0]*1;
+        m = data[1]*1;
+        x1 = data[2]*1;
+        y1 = data[3]*1;
+        x2 = data[4]*1;
+        y2 = data[5]*1;
+        col = new Array(n);
+        used = new Array(n);
+        for (let i = 0; i < n; i++) {
+            used[i] = new Array(m);
+            col[i] = new Array(m);
+            for (let j = 0; j < m; j++) {
+                used[i][j] = false;
+                col[i][j] = -1;
+            }
+        }
+        path.push([x1, y1]);
+        anspath.push([x1, y1]);
+        col[x1][y1] = depth;
+        prec = new Array(dir.length);
+        for (let id = 0; id < 8; id++) {
+            prec[id] = [];
+        }
+        for (let id = 0; id < dir.length; id++) {
+            let curx = dir[id][0], cury = dir[id][1];
+            for (let lastx = -off; lastx <= off; lastx++) {
+                for (let lasty = -off; lasty <= off; lasty++) {
+                    for (let id1 = 0; id1 < dir.length; id1++) {
+                        let nextx = lastx + dir[id1][0], nexty = lasty + dir[id1][1];
+                        if (nextx < -off || nextx > off || nexty < -off || nexty > off) continue;
+                        if (segmentsIntersect(0, 0, curx, cury, lastx, lasty, nextx, nexty)) {
+                            prec[id].push([lastx, lasty, nextx, nexty]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    function checkIntersection(lastx, lasty, id) {
+        for (let elem of prec[id]) {
+            let curx1 = elem[0] + lastx, cury1 = elem[1] + lasty, curx2 = elem[2] + lastx, cury2 = elem[3] + lasty;
+            if (!isCorrect(curx1, cury1) || !isCorrect(curx2, cury2)) {
+                continue;
+            }
+            if (col[curx1][cury1] == -1 || col[curx2][cury2] == -1) {
+                continue;
+            }
+            if (col[curx1][cury1] - col[curx2][cury2] == 1) {
                 return false;
             }
         }
@@ -55,97 +88,73 @@ function fedroFindMax(event)
     function isOk(curx, cury) {
         for (let i = 0; i < n; i++) {
             for (let j = 0; j < m; j++) {
-                used1[i][j] = false;
+                used[i][j] = false;
             }
         }
-        let q = new Array();
-        q.push([curx, cury]);
-        used1[curx][cury] = true;
+        let q = [[curx, cury]];
+        used[curx][cury] = true;
         for (let id = 0; id < q.length; id++) {
             let lastx = q[id][0], lasty = q[id][1];
             for (let d = 0; d < 8; d++) {
-                let nextx = lastx + dirx[d], nexty = lasty + diry[d];
-                if (nextx < 0 || nextx >= n || nexty < 0 || nexty >= m) continue;
-                if (used1[nextx][nexty]) continue;
-                if (used[nextx][nexty]) continue;
-                if (!checkIntersection(lastx, lasty, nextx, nexty)) continue;
+                let nextx = lastx + dir[d][0], nexty = lasty + dir[d][1];
+                if (!isCorrect(nextx, nexty)) {
+                    continue;
+                }
+                if (used[nextx][nexty] || col[nextx][nexty] != -1) {
+                    continue;
+                }
+                if (!checkIntersection(lastx, lasty, d)) {
+                    continue;
+                }
                 q.push([nextx, nexty]);
-                used1[nextx][nexty] = true;
-                if (used1[x2][y2]) break;
+                used[nextx][nexty] = true;
+                if (used[x2][y2]) break;
             }
         }
-        return used1[x2][y2];
+        return used[x2][y2];
     }
-
-    let global_ops = 0;
-    let depth = 0;
 
     function recursiveGen() {
-        let lastx = pathx[pathx.length - 1];
-        let lasty = pathy[pathy.length - 1];
+        let lastx = path[path.length - 1][0];
+        let lasty = path[path.length - 1][1];
         if (depth < 10 && !isOk(lastx, lasty)) return;
         global_ops++;
-        // if (global_ops % 500000 == 0) {
-        //     submitCurrent();
-        // }
-        // console.log(lastx, lasty, x2, y2, pathx.length);
-        if (lastx == x2 && lasty == y2 && max_answer < pathx.length - 1) {
-            max_answer = pathx.length - 1;
-            anspathx = pathx.slice(0);
-            anspathy = pathy.slice(0);
-            submitCurrentMax();
-        }
-        if (lastx == x2 && lasty == y2 && global_ops % 5000 == 0) {
-            submitCurrent();
+        if (lastx == x2 && lasty == y2) {
+            if (anspath.length < path.length) {
+                anspath = path.slice(0);
+                postMessage(path);
+            }
+            if (global_ops % 50000 == 0) {
+                postMessage(path);
+            }
             return;
         }
-        for (let rot = 0; rot < dirx.length; rot++) {
-            let nextx = lastx + dirx[rot];
-            let nexty = lasty + diry[rot];
-            if (nextx < 0 || nextx >= n || nexty < 0 || nexty >= m) continue;
-            if (used[nextx][nexty]) continue;
-            if (!checkIntersection(lastx, lasty, nextx, nexty)) continue;
-            pathx.push(nextx);
-            pathy.push(nexty);
-            used[nextx][nexty] = true;
+        for (let id = 0; id < dir.length; id++) {
+            let nextx = lastx + dir[id][0];
+            let nexty = lasty + dir[id][1];
+            if (!isCorrect(nextx, nexty)) {
+                continue;
+            }
+            if (col[nextx][nexty] != -1) {
+                continue;
+            }
+            if (!checkIntersection(lastx, lasty, id)) {
+                continue;
+            }
+            path.push([nextx, nexty]);
             depth++;
+            col[nextx][nexty] = depth;
             recursiveGen();
+            col[nextx][nexty] = -1;
             depth--;
-            used[nextx][nexty] = false;
-            pathx.pop();
-            pathy.pop();
+            path.pop();
         }
     }
+
     onmessage=function(event) {
-        n = event.data[0]*1;
-        m = event.data[1]*1;
-        x1 = event.data[2]*1;
-        y1 = event.data[3]*1;
-        x2 = event.data[4]*1;
-        y2 = event.data[5]*1;
-        dirx = new Array(1, 1, 2, 2, -1, -1, -2, -2);
-        diry = new Array(2, -2, 1, -1, 2, -2, 1, -1);
-        pathx = new Array();
-        pathy = new Array();
-        anspathx = new Array();
-        anspathy = new Array();
-        used = new Array(n);
-        used1 = new Array(n);
-        max_answer = 0;
-        for (let i = 0; i < n; i++) {
-            used[i] = new Array(m);
-            used1[i] = new Array(m);
-            for (let j = 0; j < m; j++) {
-                used[i][j] = used1[i][j] = false;
-            }
-        }
-        used[x1][y1] = true;
-        pathx.push(x1);
-        pathy.push(y1);
-        anspathx.push(x1);
-        anspathy.push(y1);
-        submitCurrentMax();
+        init(event.data);
+        postMessage(anspath);
         recursiveGen(x2, y2);
-        finish();
+        postMessage('finish');
     }
 }
